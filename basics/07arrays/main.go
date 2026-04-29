1ae7f6b7a62efe07c8c6444680d389a24146c690
Priyanshu Negi <npriyanshu63@gmail.com>
2026-04-30 01:55:33 +0530

    Add basics 07arrays

---
+++ basics/07arrays/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to array in golang")
+
+	var fruitList [4]string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Tomato"
+	fruitList[2] = "Peach"
+	// fruitList[3] = "Banana"
+
+	fmt.Println("Fruit list is", fruitList)
+
+	var vegList = [3]string{"Potato", "Beans", "Mushroom"}
+	fmt.Println("Veg list is", vegList, " and the length is", len(vegList))
+
+}
